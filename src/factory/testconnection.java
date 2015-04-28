77f1d35470e84884b8221fe1c0f9008cc323baca
lucas <lucas@10.30.40.22>
2015-04-27 22:36:53 -0300

    Nova versao

---
+++ src/factory/testconnection.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+
+public class testconnection {
+	@Test
+    public void testarConexao() throws SQLException {
+        Connection connection = new ConnectionFactory().getConnection();
+        System.out.println("Coneccao aberta!");
+			connection.close();
+    }
+}
