09adec7a99c8bfe1cbc6bbf8a565d08bc64fc437
lucas <lucas@lucas-k8>
2015-05-16 16:24:09 -0300

    comite 5

--- src/Visao/FrmClientes.java
+++ src/Visao/FrmClientes.java
@@ -662,6 +662,6 @@ public class FrmClientes extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonAdicionar;
     private javax.swing.JButton jButtonAnterior;
-    
+    private javax.swing.JButton 
     // End of variables declaration//GEN-END:variables
 }
